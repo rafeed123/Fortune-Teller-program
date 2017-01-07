0bde7b925af90f005d79a321107db51359f37119
Rafee Daanish <rafee.daanish@gmail.com>
2017-01-06 22:14:18 -0500

    on ROYGBIV step

--- Fortune Teller Program/Program.cs
+++ Fortune Teller Program/Program.cs
@@ -10,6 +10,49 @@ namespace Fortune_Teller_Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("If you want to know your future please enter your first name");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine(" Thank you " + name + "." + " Now please enter your last name");
+
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Now " + name + " " + lastName + " can you tell me how old you are");
+
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now that I know your age tell me what month you were born using a # ");
+
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("So you were born in the monnth of " + birthMonth + " very nice");
+
+            Console.WriteLine("Now can you tell me you favorite ROYGBIV color");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
